4aa9aedd97265a2a24ededda679d2fe3aa294814
papageg <greggordon98@gmail.com>
2019-05-12 17:41:59 -0700

    start at home
    start at home

--- app.js
+++ app.js
@@ -15,10 +15,3 @@ app.listen(3000);
 
 
 
-// get / - Home route should redirect to the /books route.
-// get /books - Shows the full list of books.
-// get /books/new - Shows the create new book form.
-// post /books/new - Posts a new book to the database.
-// get /books/:id - Shows book detail form.
-// post /books/:id - Updates book info in the database.
-// post /books/:id/delete - Deletes a book. Careful, this can’t be undone. It can be helpful to create a new “test” book to test deleting.
--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express.Router();
+var Book = require("../models").Book;
 
 app.get('/', (req, res, next) => {
     res.send('Welcome Home');
@@ -13,9 +14,11 @@ app.get('/', (req, res, next) => {
     res.send('New Book Form');
   });
 
-//   app.post('/books/new', (req, res, next) => {
-//     res.send('Post Book to database');
-//   });
+  app.post('/books/new', (req, res, next) => {
+    Book.create(req.body).then(function(book) {
+      res.redirect("/book/" + book.id);
+    });
+  });
 
   app.get('/books/:id', (req, res, next) => {
     res.send('Book Detail Form');
@@ -30,3 +33,12 @@ app.get('/', (req, res, next) => {
 //   });
 
 module.exports = app;
+
+
+// get / - Home route should redirect to the /books route.
+// get /books - Shows the full list of books.
+// get /books/new - Shows the create new book form.
+// post /books/new - Posts a new book to the database.
+// get /books/:id - Shows book detail form.
+// post /books/:id - Updates book info in the database.
+// post /books/:id/delete - Deletes a book. Careful, this can’t be undone. It can be helpful to create a new “test” book to test deleting.
