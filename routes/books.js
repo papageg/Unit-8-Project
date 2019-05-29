20acc3d6e6f11be5fb1422ebe1af8d889c7252a4
papageg <greggordon98@gmail.com>
2019-05-28 20:37:04 -0700

    errors saved
    errors saved

--- routes/books.js
+++ routes/books.js
@@ -78,12 +78,3 @@ var Book = require("../models").Book;
   });
 
 module.exports = router;
-
-
-// get / - Home route should redirect to the /books route.
-// get /books - Shows the full list of books.
-// get /books/new - Shows the create new book form.
-// post /books/new - Posts a new book to the database.
-// get /books/:id - Shows book detail form.
-// post /books/:id - Updates book info in the database.
-// post /books/:id/delete - Deletes a book. Careful, this can’t be undone. It can be helpful to create a new “test” book to test deleting.
