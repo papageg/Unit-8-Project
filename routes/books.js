62962a20611baf98c0e0d8f168467326c89ac406
Greg <greggordon98@gmail.com>
2019-06-03 13:14:21 -0700

    close i guess
    close i guess

--- routes/books.js
+++ routes/books.js
@@ -77,11 +77,10 @@ var Book = require("../models").Book;
           author: reqbody.author,
           genre: reqbody.genre,
           year: reqbody.year,
-          id: reqbody.id,
           errors: errors
         });
-      } else {
-        next(err);
+      } else if (req.params.action === 'edit') {
+        return book.update(reqbody);
       }
     })
   });
